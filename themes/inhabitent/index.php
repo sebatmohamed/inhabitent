005b5b5cff32cdf8817ee59f2185cf2e98cf399d
Sebat <sebat@Sebats-MacBook-Air.local>
2019-11-10 19:02:02 -0500

    style: updated content on journal page

--- themes/inhabitent/index.php
+++ themes/inhabitent/index.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
 
+<div class="journal-posts">
+
 <?php if( have_posts() ) :
 
 //The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
-    
+
+    <div class="photo-text">
     <h2><?php the_title(); ?></h2>
     <?php the_post_thumbnail('large');?>
-    <!-- <h3><?php the_permalink();?></h3> -->
-    <?php the_content(); ?>
+    </div>
+    <p><?php echo wp_trim_words(get_the_content(), 60, ' [...]');?></p>
+    <section>
+    <button>Read more <i class="fas fa-long-arrow-alt-right"></i></button>
+    </section>
     
     <!-- Loop ends -->
     <?php endwhile;?>
@@ -20,5 +26,7 @@
         <p>No posts found</p>
 <?php endif;?>
 
-    
+</div>
+
+<?php get_sidebar();?>
 <?php get_footer();?>
